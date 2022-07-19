cb8434d6c1d65ed4c1d8ed65d5bd31c05a466415
buttercarb <66517602+buttercarb@users.noreply.github.com>
2022-07-19 22:49:30 +0800

    数组下
    数组下练习

---
+++ 数组下.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+// int main()
+// {
+//     int arr[2][3] = 
+//     {
+//         {1,2,3},
+//         {4,5,6}
+//     };
+
+//     cout << sizeof(arr) << endl;
+//     cout << sizeof(arr[0]) << endl;
+//     cout << sizeof(arr[0][0]) << endl;
+
+//     cout << sizeof(arr)/sizeof(arr[0]) << endl;
+//     cout << sizeof(arr[0])/sizeof(arr[0][0]) << endl;
+
+//     cout << arr << endl;
+//     cout << arr[0] << endl;
+//     cout << arr[1] << endl;
+
+//     cout << &arr[0][0] << endl;
+//     cout << &arr[0][1] << endl;
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int scores[3][3] = 
+//     {
+//         {100, 100, 100},
+//         {90, 50, 100},
+//         {60, 70, 80}
+//     };
+
+//     string names[3] = {"张三", "李四", "王五"};
+
+//     for (int i = 0; i < 3; i++)
+//     {
+//         int sum = 0;
+//         for (int j = 0; j < 3; j++)
+//         {
+//             sum += scores[i][j];
+//         }
+//         cout << names[i] << "同学的总成绩是" << sum << endl;
+//     }
+
+//     return 0;
+// }
